e9700648d922af22110baea5a9b0a27c49877fd1
mva-odoo <mva@odoo.com>
2019-01-11 13:30:01 +0100

    remove unused module

--- syndic_dashboard/controllers/main.py
+++ syndic_dashboard/controllers/main.py
@@ -10,6 +10,20 @@ class OrderRoute(http.Controller):
     @http.route('/timeline/statistics', type='json', auth='user')
     def get_timeline(self):
         today = date.today()
+        Mois_fr = {
+            1: 'Janvier',
+            2: 'Fevrier',
+            3: 'Mars',
+            4: 'Avril',
+            5: 'Mai',
+            6: 'Juin',
+            7: 'Juillet',
+            8: 'Aout',
+            9: 'Septembre',
+            10: 'Octobre',
+            11: 'Novembre',
+            12: 'Decembre',
+        }
         dates = []
 
         dates.append((today-relativedelta(months=2)))
@@ -30,12 +44,21 @@ class OrderRoute(http.Controller):
             content = ''
             for building in buildings.filtered(lambda s: s.date_mois == month.month):
                 content += '<br/>%s (%s)' % (building.name, building.date_quizaine)
-
             events.append({
-                'date': '%s - %s' % (month.year, month.month),
+                'date': '%s - %s' % (month.year, Mois_fr.get(month.month)),
                 'content': content,
             })
 
         return {
             'myEvents': events
+        }
+
+    @http.route('/dashboard/buildings', type='json', auth='user')
+    def get_buildings(self):
+        uid = http.request.env['syndic.building']._context.get('uid')
+        return {
+            'buildings': http.request.env['syndic.building'].search_read(
+                [('manager_id', '=', uid)],
+                ['name']
+            )
         }
--- syndic_dashboard/static/src/js/dashboard.js
+++ syndic_dashboard/static/src/js/dashboard.js
@@ -9,23 +9,38 @@ var qweb = core.qweb;
 var Dashboard = AbstractAction.extend(ControlPanelMixin, {
 	template: 'dashboardTimeline',
 
+	events: {
+        'click .building_btn': '_openBuilding',
+    },
+
 	willStart: function(){
 		var self = this;
         var statsDef = this._rpc({route: '/timeline/statistics'}).then(function (stats) {
             self.stats = stats;
         });
+
+				var buildingsDef = this._rpc({route: '/dashboard/buildings'}).then(function (buildings) {
+					self.buildings = buildings;
+				});
+
         var superDef = this._super.apply(this, arguments);
-        return $.when(statsDef, superDef);
+
+        return $.when(buildingsDef, statsDef, superDef);
 	},
 
 	start: function(){
         var superDef = this._super.apply(this, arguments);
         this._renderButtons();
-        return superDef.then(this._updateControlPanel()).then(this._render_timeline.bind(this));
+
+        return superDef.then(
+					this._updateControlPanel()).then(
+						this._render_timeline.bind(this)).then(
+							this._render_building.bind(this)).then(
+								this._render_meetings.bind(this)
+							);
 	},
 
 	_render_timeline: function(){
-
 		this.$('.my-timeline').roadmap(this.stats.myEvents, {
 			eventTemplate: '<div class="event">' + '<div class="event__date">####DATE###</div>' + '<div class="event__content">####CONTENT###</div>' + '</div>',
 			eventsPerSlide: 5,
@@ -33,23 +48,61 @@ var Dashboard = AbstractAction.extend(ControlPanelMixin, {
 		});
 	},
 
-    do_show: function () {
-        this._super.apply(this, arguments);
-        this._updateControlPanel();
-    },
+	_render_building: function(){
+		var new_this = this;
+		this.buildings.buildings.forEach(function(values, key) {
+			new_this.$('.my-buildings').append('<p><a href="javascript:;" class="building_btn" data-id='+values.id+'>'+values.name+'</a></p>');
+		})
 
-    _renderButtons: function () {
-        // this.$buttons = $(qweb.render('timeline.Buttons'));
-        // this.$buttons.on('click', '.o_new_orders_btn', this._get_timeline.bind(this));
-    },
+	},
 
-    _updateControlPanel: function () {
-        this.update_control_panel({
-            cp_content: {
-               $buttons: this.$buttons,
-            },
-        });
-    },
+	_render_meetings: function(){
+		var new_this = this;
+
+		this.stats.myEvents.forEach(function(value) {
+			console.log(value);
+			var $div = $("<div>", {id: value.date, "class": "col-2"}).css('text-align', 'center');
+			$div.html('<h2>'+value.date+'</h2>');
+			new_this.$('.row.meeting_date').append($div);
+
+			var $building = $('<div>', {id: value.content,'class':"col-2"}).css('text-align', 'center');
+			$building.html(value.content);
+			new_this.$('.row.meeting_building').append($building);
+
+			// var buildings = new_this.$('.my-buildings').append('<p><a href="javascript:;" class="building_btn" data-id='+values.id+'>'+values.name+'</a></p>');
+		});
+
+	},
+
+	_openBuilding: function (event) {
+		var building_id = $(event.currentTarget).data('id');
+
+		return this.do_action({
+				res_id: building_id,
+				name: 'Immeuble',
+				res_model: 'syndic.building',
+				type: 'ir.actions.act_window',
+				views: [[false, 'form']],
+		});
+},
+
+  do_show: function () {
+      this._super.apply(this, arguments);
+      this._updateControlPanel();
+  },
+
+  _renderButtons: function () {
+      // this.$buttons = $(qweb.render('timeline.Buttons'));
+      // this.$buttons.on('click', '.o_new_orders_btn', this._get_timeline.bind(this));
+  },
+
+  _updateControlPanel: function () {
+      this.update_control_panel({
+          cp_content: {
+             $buttons: this.$buttons,
+          },
+      });
+  },
 
 });
 
